acb6e5f4fbb4621ecf009ff8cb50c5029d27b8ab
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-05 19:18:33 +0200

    Add example test suite to project

---
+++ eclipseProject/test/ocanalyzer/test/OCExampleIntegrationTest.java
@@ -0,0 +1,14 @@
+package ocanalyzer.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import ocanalyzer.test.integration.IntegrationTestElse;
+
+public class OCExampleIntegrationTest {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("org.ocanalyzer.integration tests");
+		suite.addTestSuite(IntegrationTestElse.class);
+		return suite;
+	}
+}
---
+++ eclipseProject/test/ocanalyzer/test/integration/IntegrationTestElse.java
@@ -0,0 +1,15 @@
+package ocanalyzer.test.integration;
+
+import junit.framework.TestCase;
+
+public class IntegrationTestElse extends TestCase {
+
+	public void test() {
+		fail("Initial failing test");
+	}
+
+	public void test2() {
+		fail("Initial failing test2");
+	}
+
+}
