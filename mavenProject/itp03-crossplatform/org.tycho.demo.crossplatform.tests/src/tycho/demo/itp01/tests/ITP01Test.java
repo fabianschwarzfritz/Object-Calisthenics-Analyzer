5229e91fb403d01503ee5c67a061793deccbb2d1
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-08 19:08:57 +0200

    Add maven project skeleton

---
+++ mavenProject/itp03-crossplatform/org.tycho.demo.crossplatform.tests/src/tycho/demo/itp01/tests/ITP01Test.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2011 Sonatype Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Sonatype Inc. - initial API and implementation
+ *******************************************************************************/
+package tycho.demo.itp01.tests;
+
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+import org.junit.Test;
+
+import tycho.demo.itp01.actions.SampleAction;
+
+public class ITP01Test {
+
+    @Test
+    public void sampleAction() {
+        assert(true);
+    }
+
+}
