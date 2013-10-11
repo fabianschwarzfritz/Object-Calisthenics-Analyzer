7f403fcd4237e5ece3658359fbe8119022a99fed
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-11 23:57:30 +0200

    Improve else rule example source file that is used by integration test

--- testproject/TestProject/src/elseRule/wrong/Else.java
+++ testproject/TestProject/src/elseRule/wrong/Else.java
@@ -4,9 +4,10 @@ public class Else {
 	public static void main(String[] args) {
 		String var = new String("asdf");
 		if (args[2].equals(var)) {
-
+			var = "asdf";
 		} else {
-			return;
+			var = "@@@@@";
 		}
+		return;
 	}
 }
