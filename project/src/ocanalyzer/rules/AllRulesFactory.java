8595bbc6229ec8ca88030fd9f9f3d2dd14c96a12
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-24 19:08:48 +0200

    Add documentation to factory creating all rules

--- project/src/ocanalyzer/rules/AllRulesFactory.java
+++ project/src/ocanalyzer/rules/AllRulesFactory.java
@@ -8,6 +8,12 @@ import ocanalyzer.rules.noelse.ElseFactory;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
+/**
+ * This class is a {@link RuleFactory} creating all existing rules.
+ * 
+ * @author Fabian Schwarz-Fritz
+ * 
+ */
 public class AllRulesFactory extends RuleFactory {
 
 	private RuleValidatorFactory indentationFactory;
