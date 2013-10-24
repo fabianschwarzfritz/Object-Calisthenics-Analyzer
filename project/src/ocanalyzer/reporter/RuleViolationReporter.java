63391cb08b7745824b276a6b9c2c0c06ecbbfc2b
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-24 19:19:23 +0200

    Add documentation to RuleViolationReporter

--- project/src/ocanalyzer/reporter/RuleViolationReporter.java
+++ project/src/ocanalyzer/reporter/RuleViolationReporter.java
@@ -2,6 +2,17 @@ package ocanalyzer.reporter;
 
 import org.eclipse.core.resources.IResource;
 
+/**
+ * Defines all methods for all classes getting information about rule violations
+ * that where found.
+ * 
+ * When a rule violation is found, the method
+ * {@link #reportError(IResource, int, String)} is called. It gives all
+ * registered listeners information about the found rule violation.
+ * 
+ * @author Fabian Schwarz-Fritz
+ * 
+ */
 public interface RuleViolationReporter {
 
 	public abstract void reportError(IResource resource, int line, String msg);
