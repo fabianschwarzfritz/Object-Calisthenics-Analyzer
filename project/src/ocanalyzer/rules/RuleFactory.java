3ea5f00ec0949eb383aaa109dde17032baf66610
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-24 19:05:42 +0200

    Fix documentation of factory, used to create rules

--- project/src/ocanalyzer/rules/RuleFactory.java
+++ project/src/ocanalyzer/rules/RuleFactory.java
@@ -1,17 +1,12 @@
 package ocanalyzer.rules;
 
 import ocanalyzer.reporter.RuleViolationReporter;
-import ocanalyzer.rules.noelse.ElseFactory;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 /**
- * This class is used to create concrete implementations of {@link ASTVisitors}
- * implementing the rules of the Object Calisthenics.
- * 
- * It uses concrete factories for each rule (like {@link ElseFactory} for
- * rule 2 for example) to create the concrete visitors that validate a rule.
+ * This class is used to create {@link Rules}
  * 
  * @author Fabian Schwarz-Fritz
  * 
