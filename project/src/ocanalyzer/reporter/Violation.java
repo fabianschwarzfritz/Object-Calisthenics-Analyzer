1996fd326e70b6dcf7223590ddc3823bc8b83b7a
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-10-24 19:16:04 +0200

    Add documentation to violation class

--- project/src/ocanalyzer/reporter/Violation.java
+++ project/src/ocanalyzer/reporter/Violation.java
@@ -2,6 +2,15 @@ package ocanalyzer.reporter;
 
 import org.eclipse.core.resources.IResource;
 
+/**
+ * This class represents a rule violation. The location of the violation is
+ * indicated by: **a given {@link #resource} **a given line. The line in which
+ * the error occurred **a message, containing an information about the rule
+ * violation.
+ * 
+ * @author Fabian Schwarz-Fritz
+ * 
+ */
 public class Violation {
 
 	private IResource resource;
