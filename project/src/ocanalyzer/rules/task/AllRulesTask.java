25904cb39b5c2b68d0fb24973a972c051ddd8b5f
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-11-03 16:05:08 +0100

    Task validating all rules uses no reporter for determination if a given class is a wrapper class

--- project/src/ocanalyzer/rules/task/AllRulesTask.java
+++ project/src/ocanalyzer/rules/task/AllRulesTask.java
@@ -3,10 +3,9 @@ package ocanalyzer.rules.task;
 import java.util.List;
 import java.util.Set;
 
+import ocanalyzer.reporter.NoReporter;
 import ocanalyzer.reporter.RuleViolationReporter;
-import ocanalyzer.rules.RuleFactory;
 
-import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
@@ -26,7 +25,11 @@ public class AllRulesTask extends ValidationTask {
 		preTask = new PreValidationTask(unitsToAnalyze, reporter);
 		preTask.execute();
 
-		primitiveTask = new PrimitiveTypeTask(unitsToAnalyze, reporter);
+		// This Task uses a no reporter.
+		// Getting an information that the type is a wrapper class does _not_
+		// indicate that this is a rule violation. Therefore informing the user
+		// with a reporter is not necessary.
+		primitiveTask = new PrimitiveTypeTask(unitsToAnalyze, new NoReporter());
 		primitiveTask.execute();
 		Set<TypeDeclaration> wrappers = primitiveTask.getWrappers();
 
