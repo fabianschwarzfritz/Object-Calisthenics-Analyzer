8a4d5b0ce345246e5a0921c93aaad4d208618e87
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-11-20 22:16:49 +0100

    Add package violation to view

--- project/src/objectcalisthenicsvalidator/views/ViewContentProvider.java
+++ project/src/objectcalisthenicsvalidator/views/ViewContentProvider.java
@@ -3,24 +3,32 @@ package objectcalisthenicsvalidator.views;
 import java.util.ArrayList;
 import java.util.List;
 
-import ocanalyzer.reporter.ClassReporter;
+import ocanalyzer.reporter.PackageViolation;
+import ocanalyzer.reporter.Reporter;
 import ocanalyzer.reporter.Violation;
 
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 public class ViewContentProvider implements IStructuredContentProvider,
-		ClassReporter {
+		Reporter {
 
+	private List<PackageViolation> packageViolations;
 	private List<Violation> violations;
 
 	public ViewContentProvider() {
 		violations = new ArrayList<Violation>();
+		packageViolations = new ArrayList<PackageViolation>();
 	}
 
 	@Override
 	public void reportError(Violation violation) {
 		violations.add(violation);
+	}
+
+	@Override
+	public void reportError(PackageViolation violation) {
+		packageViolations.add(violation);
 	}
 
 	public void inputChanged(Viewer v, Object oldInput, Object newInput) {
@@ -30,11 +38,15 @@ public class ViewContentProvider implements IStructuredContentProvider,
 	}
 
 	public Object[] getElements(Object parent) {
-		return violations.toArray();
+		List<Object> result = new ArrayList<Object>();
+		result.addAll(violations);
+		result.addAll(packageViolations);
+		return result.toArray();
 	}
 
 	public void clear() {
 		violations = new ArrayList<Violation>();
+		packageViolations = new ArrayList<PackageViolation>();
 	}
 
 }
