530af745516f77daa1bc355833a5100d0268d402
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-11-23 20:29:29 +0100

    Add construcutors for rules by using static methods

--- project/src/ocanalyzer/rules/impl/OCRulesImpl.java
+++ project/src/ocanalyzer/rules/impl/OCRulesImpl.java
@@ -2,14 +2,41 @@ package ocanalyzer.rules.impl;
 
 import java.util.Collection;
 
+import ocanalyzer.reporter.ClassReporter;
 import ocanalyzer.rules.OCRule;
 import ocanalyzer.rules.OCRules;
+import ocanalyzer.rules.r1_indentation.RuleIndentation;
+import ocanalyzer.rules.r2_noelse.RuleElse;
+import ocanalyzer.rules.r3_8_wrap.primitives.RuleWrapPrimitives;
+import ocanalyzer.rules.r4_onedot.RuleOneDotPerLine;
+import ocanalyzer.rules.r6_small.RuleSmallEntities;
+import ocanalyzer.rules.r7_instanceVariable.RuleInstanceVariable;
+import ocanalyzer.rules.r8_wrapCollections.RuleWrapCollections;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 
 public class OCRulesImpl implements OCRules {
 
 	public Collection<OCRule> rules;
+
+	private OCRulesImpl() {
+	}
+
+	public static OCRulesImpl create() {
+		return new OCRulesImpl();
+	}
+
+	public static OCRulesImpl createStandardRules(ClassReporter reporter) {
+		OCRulesImpl rules = new OCRulesImpl();
+		rules.add(new RuleIndentation(reporter));
+		rules.add(new RuleElse(reporter));
+		rules.add(new RuleWrapPrimitives(reporter));
+		rules.add(new RuleOneDotPerLine(reporter));
+		rules.add(new RuleSmallEntities(reporter));
+		rules.add(new RuleInstanceVariable(reporter));
+		rules.add(new RuleWrapCollections(reporter));
+		return rules;
+	}
 
 	public void apply(Collection<ICompilationUnit> compilationUnits) {
 		for (OCRule rule : rules) {
