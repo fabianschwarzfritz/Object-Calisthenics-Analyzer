b74b02354d9d75959f198864ed27fbe04c972a36
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-11-23 20:11:08 +0100

    Apply architecture for package of rule 2

---
+++ project/src/ocanalyzer/rules/r2_noelse/RuleElse.java
@@ -0,0 +1,27 @@
+package ocanalyzer.rules.r2_noelse;
+
+import ocanalyzer.reporter.ClassReporter;
+import ocanalyzer.rules.general.ClassOCRuleImpl;
+import ocanalyzer.rules.general.ValidationHandler;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+public class RuleElse extends ClassOCRuleImpl {
+
+	private ClassReporter reporter;
+
+	public RuleElse(ClassReporter reporter) {
+		this.reporter = reporter;
+	}
+
+	@Override
+	public void applyIntentiationRule(ICompilationUnit iUnit,
+			CompilationUnit unit) {
+		ValidationHandler instanceValidationHandler = new ElseValidationHandler(
+				iUnit, unit, reporter);
+		ElseVisitor visitor = new ElseVisitor(instanceValidationHandler);
+		unit.accept(visitor);
+	}
+
+}
