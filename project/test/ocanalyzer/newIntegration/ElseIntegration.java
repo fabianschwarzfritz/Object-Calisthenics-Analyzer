8acec4e6c1720e69ee003eaebb70ddb670b70dec
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-11-27 22:31:59 +0100

    Add assertions to else test

--- project/test/ocanalyzer/newIntegration/ElseIntegration.java
+++ project/test/ocanalyzer/newIntegration/ElseIntegration.java
@@ -1,5 +1,11 @@
 package ocanalyzer.newIntegration;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.List;
+
+import ocanalyzer.reporter.ClassViolation;
 import ocanalyzer.rules.general.OCRule;
 import ocanalyzer.rules.impl.OCRulesImpl;
 import ocanalyzer.rules.r2_noelse.RuleElse;
@@ -25,6 +31,17 @@ public class ElseIntegration extends OCIntegration {
 	@Test
 	public void test() {
 		rules.apply(units);
+
+		List<ClassViolation> violations = reporter.getViolations();
+
+		assertSame(1, violations.size());
+		ClassViolation violation = violations.get(0);
+		assertEquals("Error when validating no-else rule. Wrong resource",
+				"ElseWrong.java", violation.getResource().getName());
+		assertEquals("Error when validating no-else rule. Wring position",
+				new Integer(8), violation.getLine());
+		assertEquals("Error when validating no-else rule. Wring message",
+				"The else keyword violates rule 2", violation.getMessage());
 	}
 
 }
