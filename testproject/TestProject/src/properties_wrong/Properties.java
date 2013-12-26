ea30e4eb56dd6d505380e420d0f9dd3bd1965605
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2013-12-26 17:56:18 +0100

    Add integration test class representing a violation of rule 9

---
+++ testproject/TestProject/src/properties_wrong/Properties.java
@@ -0,0 +1,14 @@
+package properties_wrong;
+
+public class Properties {
+
+	private String property;
+
+	public Properties() {
+		property = "somevalue";
+	}
+
+	public String getProperty() {
+		return property;
+	}
+}
