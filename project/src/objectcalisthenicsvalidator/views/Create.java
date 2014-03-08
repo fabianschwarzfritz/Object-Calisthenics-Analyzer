065fb0fd2dd93279ae0878882d9a1af599f6962f
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2014-03-08 11:38:03 +0100

    Remove adding package reporters

--- project/src/objectcalisthenicsvalidator/views/Create.java
+++ project/src/objectcalisthenicsvalidator/views/Create.java
@@ -3,21 +3,10 @@
  */
 package objectcalisthenicsvalidator.views;
 
-import objectcalisthenicsvalidator.views.column.TableColumns;
-import objectcalisthenicsvalidator.views.search.SearchAdapter;
-import objectcalisthenicsvalidator.views.search.ViolationFilter;
-import objectcalisthenicsvalidator.views.table.AddSortAction;
 import objectcalisthenicsvalidator.views.table.ViolationProvider;
 import ocanalyzer.reporter.impl.ConsoleReporter;
 import ocanalyzer.reporter.impl.DelegateReporter;
 import ocanalyzer.reporter.impl.MarkerReporter;
-
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
 /**
  * @author Fabian Schwarz-Fritz
@@ -29,10 +18,8 @@ public class Create {
 		DelegateReporter reporter = new DelegateReporter();
 		reporter.addClassReporter(new MarkerReporter());
 		reporter.addClassReporter(new ConsoleReporter(System.out));
-		reporter.addPackageReporter(new ConsoleReporter(System.out));
 		for (ViolationProvider violationProvider : providers) {
 			reporter.addClassReporter(violationProvider);
-			reporter.addPackageReporter(violationProvider);
 		}
 		return reporter;
 	}
