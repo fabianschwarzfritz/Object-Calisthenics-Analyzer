74b8545c855faf6002c5e5c315f379fe252fb98d
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2014-03-08 13:09:59 +0100

    Integrate run of validation into domain model

--- project/src/ocanalyzer/domain/RunImpl.java
+++ project/src/ocanalyzer/domain/RunImpl.java
@@ -4,12 +4,22 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import ocanalyzer.extractor.Extractor;
+import ocanalyzer.extractor.impl.ExtractorImpl;
+import ocanalyzer.reporter.TrainingReporter;
+import ocanalyzer.rules.general.ICompilationUnits;
+import ocanalyzer.rules.impl.OCRulesImpl;
+
 public class RunImpl implements Run {
 
 	private RunImpl previous;
 	private RunImpl next;
+
 	private Date time;
 	private List<Violation> violations;
+
+	private Extractor extractor;
+	private TrainingReporter trainingReporter;
 
 	RunImpl() {
 		previous = this;
@@ -24,10 +34,12 @@ public class RunImpl implements Run {
 	private void init() {
 		violations = new ArrayList<Violation>();
 		time = new Date();
+		extractor = new ExtractorImpl();
 	}
 
 	public Run update() {
 		RunImpl newRun = new RunImpl(this);
+		trainingReporter = new TrainingReporter(newRun);
 		this.next = newRun;
 		newRun.previous = this;
 		return newRun;
@@ -39,5 +51,11 @@ public class RunImpl implements Run {
 
 	public void addViolation(Violation violation) {
 		violations.add(violation);
+	}
+
+	public void validate() {
+		OCRulesImpl rules = OCRulesImpl.createStandardRules(trainingReporter);
+		ICompilationUnits units = extractor.extract();
+		rules.apply(units);
 	}
 }
--- project/src/ocanalyzer/domain/Training.java
+++ project/src/ocanalyzer/domain/Training.java
@@ -1,12 +1,9 @@
 package ocanalyzer.domain;
 
-import ocanalyzer.reporter.TrainingReporter;
-
 public class Training {
 
 	private Project project;
 	private Run lastRun;
-	private TrainingReporter trainingReporter;
 
 	public Training(Project project) {
 		this.project = project;
@@ -15,7 +12,6 @@ public class Training {
 
 	public Run create() {
 		Run update = lastRun.update();
-		trainingReporter = new TrainingReporter(update);
 		lastRun = update;
 		return update;
 	}
